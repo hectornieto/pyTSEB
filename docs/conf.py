c203548e0d770dc1af88d3e151e1fa934a131179
Héctor Nieto <hector.nieto.solana@gmail.com>
2021-05-14 13:26:39 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../pyTSEB'))
+sys.path.insert(0, os.path.abspath('../'))
 from recommonmark.parser import CommonMarkParser
 
 # -- Project information -----------------------------------------------------
